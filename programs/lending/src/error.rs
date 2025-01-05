10278ce509bd3cf6fb1031a377f676a89877d5c6
loopsyntax <softechstar@outlook.com>
2025-01-06 00:03:55 +0400

    Update the error message

--- programs/lending/src/error.rs
+++ programs/lending/src/error.rs
@@ -8,4 +8,6 @@ pub enum ErrorCode {
     OverBorrowableAmount,
     #[msg("Requested amount exceeds depositable amount")]
     OverRepay,
+    #[msg("User is not under collaterized, can't be liquidated")]
+    NotUnderCollaterized,
 }
